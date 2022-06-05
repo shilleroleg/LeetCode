061280b07c58ec4abe54a3e97330584468592456
olega <shilleroleg@gmail.com>
2022-06-05 22:54:55 +0700

    Task Sort array by parity

---
+++ src/arrays/sort_array_by_parity.py
@@ -0,0 +1,42 @@
+"""
+Sort Array By Parity
+
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+
+    1 <= nums.length <= 5000
+    0 <= nums[i] <= 5000
+"""
+from typing import List
+
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        even = []
+        odd = []
+        for num in nums:
+            if num % 2 == 0:
+                even.append(num)
+            else:
+                odd.append(num)
+        return even + odd
+
+
+if __name__ == '__main__':
+    arr = [3,1,2,4]
+
+    print(Solution().sortArrayByParity(arr))  # -> [2,4,3,1]
